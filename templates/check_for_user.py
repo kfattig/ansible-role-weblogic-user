9e6d370e9ba303c0f10a2e67c5ff92b12ee5cb05
Kyle Fattig <ansible@tsen1t011.hcqis.org>
2018-09-19 13:25:49 -0400

    added support for check_for_user

---
+++ templates/check_for_user.py
@@ -0,0 +1,10 @@
+import sys
+
+connect('{{ weblogic_admin }}','{{ weblogic_password }}','{{ t3_url }}')
+edit()
+serverConfig()
+
+cd('/SecurityConfiguration/{{ domain_name }}/Realms/myrealm/AuthenticationProviders/DefaultAuthenticator')
+sys.exit(cmo.userExists({{ username }}))
+disconnect()
+exit()
